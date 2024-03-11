abab59062b996795c9e772cf0af2ef7e8eb31dc2
Abdullah Ayaz <106861398+abd529@users.noreply.github.com>
2024-03-11 18:08:41 +0500

    font integrated

--- build/flutter_assets/assets/images/tiled.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.2" name="try" tilewidth="32" tileheight="32" tilecount="880" columns="22">
- <image source="try.png" width="720" height="1280"/>
-</tileset>
